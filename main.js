555e6d35314c0cbf065841b4f7975d58b6ce4a6e
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-06 02:49:05 +0100

    dom selections

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const navBtn = document.querySelector('.toggle-btn');
+const links = document.querySelectorAll('.link');
+const pageContainer = document.querySelector('.page-container');
+const pages = document.querySelectorAll('.pages');
 
+let timings = [];
+
+for(let i = 0; i < links.length; i++) {
+  timings.push(i*200);
+}
